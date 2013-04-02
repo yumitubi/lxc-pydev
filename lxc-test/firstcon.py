36da1d0d1b8f3fce32efd7dc68e0c745a50f29ce
Mak Tomilov <mak.tomilov@gmail.com>
2013-04-02 22:55:30 +0600

    add first test

---
+++ lxc-test/firstcon.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3.3
+# -*- coding: utf-8 -*-
+
+import lxc
+
+
+CONTAINER_NAME = "mak_test"
+TEMPLATE = "debian"
+
+def create_container(CONTAINER_NAME, TEMPLATE):
+    """initalizating new container
+
+    Arguments:
+    - `CONTAINER_NAME`:
+    - `TEMPLATE`:
+    """
+    # create instance
+    print("Getting instance for '%s'" % CONTAINER_NAME)
+    container = lxc.Container(CONTAINER_NAME)
+    container.config_file_name == "%s/%s/config" % (lxc.default_config_path,
+                                                    CONTAINER_NAME)
+    container.create(TEMPLATE)
+    return container.name
+
+
+if __name__ == '__main__':
+    create_container()
