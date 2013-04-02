7cb0156131a05cffa8184063b3bde9e261e20719
Mak Tomilov <mak.tomilov@gmail.com>
2013-04-02 23:25:58 +0600

    add start container and connect to console

--- lxc-test/firstcon.py
+++ lxc-test/firstcon.py
@@ -20,9 +20,36 @@ def create_container(CONTAINER_NAME, TEMPLATE):
     container = lxc.Container(CONTAINER_NAME)
     container.config_file_name == "%s/%s/config" % (lxc.default_config_path,
                                                     CONTAINER_NAME)
+    # create root filesystem
     container.create(TEMPLATE)
-    return container.name
+    return container
+
+
+def start_container(container):
+    """
+
+    Arguments:
+    - `container`:
+    """
+    try:
+        container.start()
+        container.wait("RUNNING", 3)
+        return True
+    except:
+        return False
+
+
+def connect_to_console(container):
+    """
+
+    Arguments:
+    - `container`:
+    """
+    print("Attaching to tty1")
+    container.console(tty=1)
 
 
 if __name__ == '__main__':
-    create_container(name, template)
+    new_container = create_container(name, template)
+    start_container(new_container)
+    connect_to_console(new_container)
